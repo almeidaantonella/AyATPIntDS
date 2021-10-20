2496124a5d9280c4e57b80dc47a6e5710c35f2c3
antonellaalmeida <antonellaalmeida@gmail.com>
2021-10-20 15:46:22 -0300

    Ver 0.051

--- www/eliminar.php
+++ www/eliminar.php
@@ -5,6 +5,7 @@ require_once 'clases/RepositorioUsuario.php';
 require_once 'clases/Libro.php';
 session_start();
 if (isset($_SESSION['usuario']) && isset($_GET['l'])) {
+    
 $usuario = unserialize($_SESSION['usuario']);
 $rl = new RepositorioLibro();
 $libro = $rl->get_one($_GET['l']);
